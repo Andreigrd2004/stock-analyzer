1ed45f985a056c2da33849b234fea2a80cc1be5a
Andreigrd2004 <andreigrd2@gmail.com>
2026-02-07 23:24:53 +0200

    Initialized project, database and relational mapping.

---
+++ src/main/java/com/analyzer/analyzer/prediction/Prediction.java
@@ -0,0 +1,48 @@
+package com.analyzer.analyzer.prediction;
+
+import com.analyzer.analyzer.stock.Stock;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "predictions", schema = "public")
+public class Prediction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stock_id")
+    private Stock stock;
+
+    @Column(name = "summary", length = Integer.MAX_VALUE)
+    private String summary;
+
+    @Size(max = 20)
+    @Column(name = "action", length = 20)
+    private String action;
+
+    @Column(name = "current_price")
+    private Double currentPrice;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @Column(name = "valid_until")
+    private Instant validUntil;
+
+    @OneToMany(mappedBy = "prediction")
+    private Set<PredictionAccuracy> predictionAccuracies = new LinkedHashSet<>();
+
+}
---
+++ src/main/java/com/analyzer/analyzer/prediction/PredictionAccuracy.java
@@ -0,0 +1,27 @@
+package com.analyzer.analyzer.prediction;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "prediction_accuracy", schema = "public")
+public class PredictionAccuracy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "prediction_id")
+    private Prediction prediction;
+
+    @Column(name = "price_after_5_days")
+    private Double priceAfter5Days;
+
+    @Column(name = "is_accurate")
+    private Boolean isAccurate;
+
+}
